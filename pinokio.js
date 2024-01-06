46b5dacc0712da8dcd31ee74b22fd03e0039adec
Kenneth Caple <califazken@gmail.com>
2024-01-06 01:18:04 -0700

    update pinokio.js

--- pinokio.js
+++ pinokio.js
@@ -1,49 +1,37 @@
-module.exports = async kernel => {
-  const config = {
-      requires: [
-          {
-              type: 'conda',
-              name: ['ffmpeg', 'spleeter'],
-              args: '-c conda-forge'
-          }
-      ],
-      run: [
-          {
-              method: 'input.file',
-              params: {
-                  title: 'Upload an audio file',
-                  description: 'Please upload the audio file you want to process with Spleeter.',
-                  accept: '.mp3, .wav'
-              }
-          },
-          {
-              method: 'input.select',
-              params: {
-                  title: 'Select the Spleeter separation model',
-                  description: 'Choose the number of stems for separation.',
-                  options: ['2stems', '4stems', '5stems']
-              }
-          },
-          {
-              method: 'shell.run',
-              params: {
-                  message: 'spleeter separate -o output/ -p spleeter:${model} ${input}',
-                  env: {
-                      model: '${input.select[1]}',
-                      input: '${input.file[0].path}'
-                  }
-              }
-          },
-          {
-              method: 'output.directory',
-              params: {
-                  path: 'output/',
-                  title: 'Processed Files',
-                  description: 'Download the separated audio tracks.'
-              }
-          }
-      ]
-  };
-
-  return config;
-};
+module.exports = {
+  title: "Spleeter",
+  icon: "spleeter-icon.jpeg",
+  description: "Efficiently separate audio tracks with Spleeter",
+  menu: async (kernel) => {
+    let installed = await kernel.exists(__dirname, "Spleeter", "venv")
+    if (installed) {
+      let session = await kernel.require(__dirname, "session.json")
+      let running = await kernel.running(__dirname, "start.json")
+      if (running) {
+        return [{
+          icon: "fa-solid fa-spin fa-circle-notch",
+          text: "Running"
+        }, {
+          icon: "fa-solid fa-rocket",
+          text: "Open UI",
+          href: (session && session.url ? session.url : "http://127.0.0.1:8080"),
+          target: "_blank"
+        }]
+      } else {
+        return [{
+          icon: "fa-solid fa-power-off",
+          text: "Launch",
+          href: "start.json",
+          params: { fullscreen: true, run: true }
+        }]
+      }
+    } else {
+      return [{
+        icon: "fa-solid fa-plug",
+        text: "Install",
+        href: "install.json",
+        params: { run: true, fullscreen: true }
+      }]
+    }
+  }
+}
